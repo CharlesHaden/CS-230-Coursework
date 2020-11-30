a3208d5b07afc0bce77c0b537d9d55d544e45678
CharlesHaden <68151344+CharlesHaden@users.noreply.github.com>
2020-11-30 01:43:48 +0000

    added git profile

---
+++ src/Profile.java
@@ -0,0 +1,45 @@
+/**
+ * A class for making profiles
+ *
+ * @author Charles Haden
+ * @author Mathew Clarke
+ */
+public class Profile {
+    private int wins;
+    private int losses;
+    private String name;
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Changes name
+     * @param name - name to be changed to.
+     */
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+}
+
