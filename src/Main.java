41900fd47785ebdd3bcb1c56fb18945975350733
nbman07 <1904848@swansea.ac.uk>
2020-12-03 19:58:33 +0000

    added setup screen

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-
+/** 
+ * 
+ * 
+ * @author Nim Man
+*/
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,10 +12,15 @@ import javafx.scene.text.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
 
 
  
 public class Main extends Application {
+   // Constants for the main window
+	private static final int MAIN_WINDOW_WIDTH = 700;
+	private static final int MAIN_WINDOW_HEIGHT = 600;
+   private static final String WINDOW_TITLE = "Fast and Curious!";
    Stage window;
    Scene mainMenu;
    Scene pauseMenu;
@@ -19,39 +28,40 @@ public class Main extends Application {
    Scene leaderboardScreen;
    Scene inGameScreen;
 
+    
     @Override
     public void start(Stage window) throws Exception {
 
       //MAIN MENU
       Pane root = new Pane(); 
-      mainMenu = new Scene(root, 500, 550);
-      Text t = new Text(80, 100, "Fast and Curious");
-      t.setFont(new Font(50));
-      root.getChildren().add(t);    
+      mainMenu = new Scene(root, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+      Text title = new Text(160, 100, WINDOW_TITLE);
+      title.setFont(new Font(50));
+      root.getChildren().add(title);    
 
       //CREATE BUTTONS
        Button playButton = new Button("Play");
        Button leaderboardButton = new Button("Leaderboard");
        Button exitButton = new Button("Exit");
 
-       playButton.setLayoutX(200);
+       playButton.setLayoutX(300);
        playButton.setLayoutY(260);
        playButton.setPrefSize(100,20);
        root.getChildren().add(playButton);
 
-       leaderboardButton.setLayoutX(200);
+       leaderboardButton.setLayoutX(300);
        leaderboardButton.setLayoutY(300);
        leaderboardButton.setPrefSize(100,20);
        root.getChildren().add(leaderboardButton);
 
-       exitButton.setLayoutX(200);
+       exitButton.setLayoutX(300);
        exitButton.setLayoutY(340);
        exitButton.setPrefSize(100,20);
        root.getChildren().add(exitButton);
        
        
        //SET BUTTON ACTIONS
-       playButton.setOnAction(e -> window.setScene(inGameScreen));
+       playButton.setOnAction(e -> window.setScene(setup));
        leaderboardButton.setOnAction(e -> window.setScene(leaderboardScreen));
        exitButton.setOnAction(new EventHandler<ActionEvent>() {
           @Override
@@ -63,36 +73,97 @@ public class Main extends Application {
       //INGAME SCREEN
       
       Pane game = new Pane();
-      inGameScreen = new Scene(game, 500, 550);
-      Button newButton = new Button("Back to Main Menu");
-      newButton.setOnAction(e -> window.setScene(mainMenu));
-      game.getChildren().add(newButton); //ADDED BUTTON TO TEST NAVIGATION 
+      inGameScreen = new Scene(game, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
       
+      Image imageDecline = new Image("pause.png");
+      Button pauseButton = new Button();
+      ImageView imageDeclineView = new ImageView(imageDecline);
+      imageDeclineView.setFitHeight(20);
+      imageDeclineView.setFitWidth(20);
+      pauseButton.setGraphic(imageDeclineView);
+      pauseButton.setPrefSize(100,20);
+      pauseButton.setOnAction(e -> window.setScene(pauseMenu));
+      game.getChildren().add(pauseButton);
+
       //PAUSE MENU
+
+      Pane pause = new Pane();
+      pauseMenu = new Scene(pause, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+      Text paused = new Text(230, 150, "Game Paused");
+      paused.setFont(new Font(40));
+      pause.getChildren().add(paused); 
+
+      Button resumeButton = new Button("Resume game");
+      resumeButton.setOnAction(e -> window.setScene(inGameScreen));
+      resumeButton.setLayoutX(300);
+      resumeButton.setLayoutY(260);
+      resumeButton.setPrefSize(100,20);
+      pause.getChildren().add(resumeButton); //ADDED BUTTON TO TEST NAVIGATION 
+      
+      Button saveGame = new Button("Save and Exit");
+      //NEED TO ADD SAVE FUNCTION
+      saveGame.setOnAction(e -> window.setScene(mainMenu));
+      saveGame.setLayoutX(300);
+      saveGame.setLayoutY(300);
+      saveGame.setPrefSize(100,20);
+      pause.getChildren().add(saveGame);
       
       //LEADERBOARD
       
       Pane leaderboard = new Pane();
-      leaderboardScreen = new Scene(leaderboard, 500, 550);
+      leaderboardScreen = new Scene(leaderboard, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
       Button newButton2 = new Button("Return to Main Menu");
       newButton2.setOnAction(e -> window.setScene(mainMenu));
       leaderboard.getChildren().add(newButton2); //ADDED BUTTON TO TEST NAVIGATION 
 
       //SETUP/PROFILE
 
+      Pane profileSetup = new Pane();
+      setup = new Scene(profileSetup, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+      Text settings = new Text(280, 100, "Settings");
+      settings.setFont(new Font(40));
+      profileSetup.getChildren().add(settings);
+      Text players = new Text(150, 200, "How many players are there?");
+      players.setFont(new Font(30));
+      profileSetup.getChildren().add(players);
       
 
+      Button twoPlayer = new Button("2");
+      twoPlayer.setLayoutX(100);
+      twoPlayer.setLayoutY(260);
+      twoPlayer.setPrefSize(100,20);
+      profileSetup.getChildren().add(twoPlayer); 
+      Button threePlayer = new Button("3");
+      threePlayer.setLayoutX(300);
+      threePlayer.setLayoutY(260);
+      threePlayer.setPrefSize(100,20);
+      profileSetup.getChildren().add(threePlayer); 
+      Button fourPlayer = new Button("4");
+      fourPlayer.setLayoutX(500);
+      fourPlayer.setLayoutY(260);
+      fourPlayer.setPrefSize(100,20);
+      profileSetup.getChildren().add(fourPlayer); 
+
+      Button startGame = new Button("Start game");
+      startGame.setLayoutX(550);
+      startGame.setLayoutY(550);
+      startGame.setPrefSize(100,20);
+      profileSetup.getChildren().add(startGame); 
+      startGame.setOnAction(e -> window.setScene(inGameScreen));
+
+      Button backButton = new Button("Back to Main Menu");
+      backButton.setPrefSize(100,20);
+      profileSetup.getChildren().add(backButton); 
+      backButton.setOnAction(e -> window.setScene(mainMenu));
 
       //DEFAULT STARTUP SETTINGS
-
-      
       //ICON
       window.getIcons().add(new Image("cars.jpg"));
-
-      window.setTitle("Fast and Curious!");
+      window.setTitle(WINDOW_TITLE);
       window.setScene(mainMenu);
       window.show();
    }
+   
    
    public static void main(String[] args) {
         launch(args);
