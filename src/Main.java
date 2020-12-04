d1a4121e9b2241ecd27300d944ca8ef77395a86e
nbman07 <1904848@swansea.ac.uk>
2020-12-04 19:53:53 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.text.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
@@ -30,12 +31,15 @@ public class Main extends Application {
    Scene setup;
    Scene leaderboardScreen;
    Scene inGameScreen;
+   Scene loadGameScreen;
 
     
     @Override
     public void start(Stage window) throws Exception {
 
       //MAIN MENU
+    //  MainMenu gameMenu = new MainMenu();
+
       Pane root = new Pane(); 
       mainMenu = new Scene(root, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
       Text title = new Text(160, 100, WINDOW_TITLE);
@@ -44,27 +48,43 @@ public class Main extends Application {
 
       //CREATE BUTTONS
        Button playButton = new Button("Play");
+       Button loadButton = new Button("Load game");
        Button leaderboardButton = new Button("Leaderboard");
        Button exitButton = new Button("Exit");
+       
 
        playButton.setLayoutX(300);
        playButton.setLayoutY(260);
        playButton.setPrefSize(100,20);
        root.getChildren().add(playButton);
 
+       loadButton.setLayoutX(300);
+       loadButton.setLayoutY(300);
+       loadButton.setPrefSize(100,20);
+       root.getChildren().add(loadButton);
+
+
        leaderboardButton.setLayoutX(300);
-       leaderboardButton.setLayoutY(300);
+       leaderboardButton.setLayoutY(340);
        leaderboardButton.setPrefSize(100,20);
        root.getChildren().add(leaderboardButton);
 
+
        exitButton.setLayoutX(300);
-       exitButton.setLayoutY(340);
+       exitButton.setLayoutY(380);
        exitButton.setPrefSize(100,20);
        root.getChildren().add(exitButton);
-       
-       
+
        //SET BUTTON ACTIONS
        playButton.setOnAction(e -> window.setScene(setup));
+       //loadButton.setOnAction(e -> MainMenu.loadBoard());
+       loadButton.setOnAction(new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent event) {
+            window.setScene(loadGameScreen);
+            MainMenu.loadBoard();
+           }
+     });
        leaderboardButton.setOnAction(e -> window.setScene(leaderboardScreen));
        exitButton.setOnAction(new EventHandler<ActionEvent>() {
           @Override
@@ -75,8 +95,12 @@ public class Main extends Application {
       });
       //MESSAGE OF THE DAY
       MessageOfTheDay newMessage = new MessageOfTheDay(); 
-      Text messageOtD = new Text(10, 500, newMessage.getMessage());
-      messageOtD.setFont(new Font(12));
+      Label messageOtD = new Label(newMessage.getMessage());
+      messageOtD.setFont(new Font(15));
+      messageOtD.setLayoutX(10);
+      messageOtD.setLayoutY(500);
+      messageOtD.setPrefWidth(700);
+      messageOtD.setWrapText(true);
       root.getChildren().add(messageOtD); 
 
 
@@ -90,6 +114,50 @@ public class Main extends Application {
       pauseButton.setGraphic(imageDeclineView);
       pauseButton.setPrefSize(100,20);
       pauseButton.setOnAction(e -> window.setScene(pauseMenu));
+
+      Label insertPrompt = new Label("Insert tile into board");
+      insertPrompt.setFont(new Font(10));
+      insertPrompt.setLayoutX(510);
+      insertPrompt.setLayoutY(370);
+      insertPrompt.setPrefWidth(400);
+      Label availableAction = new Label("Available Actions:");
+      availableAction.setFont(new Font(15));
+      availableAction.setLayoutX(510);
+      availableAction.setLayoutY(420);
+      availableAction.setPrefWidth(400);
+
+
+      Label playerTurnLabel = new Label("Player's turn");
+      playerTurnLabel.setFont(new Font(30));
+      playerTurnLabel.setLayoutX(480);
+      playerTurnLabel.setLayoutY(100);
+      playerTurnLabel.setPrefWidth(400);
+
+      Button drawTile = new Button("Draw Tile");
+      drawTile.setPrefSize(90, 20);
+      drawTile.setLayoutX(490);
+      drawTile.setLayoutY(200);
+      
+
+      Button playAction = new Button("Play Action");
+      playAction.setPrefSize(90, 20);
+      playAction.setLayoutX(580);
+      playAction.setLayoutY(200);
+      playAction.setDisable(true);
+      Button endTurnButton = new Button("End turn");
+      endTurnButton.setPrefSize(90, 20);
+      endTurnButton.setLayoutX(580);
+      endTurnButton.setLayoutY(550);
+      endTurnButton.setDisable(true);
+
+      Button rotateRight = new Button("R");
+      rotateRight.setPrefSize(20, 20);
+      rotateRight.setLayoutX(640);
+      rotateRight.setLayoutY(300);
+      Button rotateLeft = new Button("L");
+      rotateLeft.setPrefSize(20, 20);
+      rotateLeft.setLayoutX(490);
+      rotateLeft.setLayoutY(300);
       
 
       Image straightTile = new Image("straight.PNG");
@@ -97,8 +165,9 @@ public class Main extends Application {
       Image tShapedTile = new Image("tshaped.PNG");
       Image goalTile = new Image("goal.PNG");
 
-      FloorTile[][] curboard = new FloorTile[10][10];
-      for (int i = 0; i < 10; i++) {
+      //FloorTile[][] curboard = MainMenu.loadPresetBoard(1);
+      FloorTile[][] curboard = new FloorTile[10][10]; 
+      for (int i = 0; i < 10; i++) {  
          for (int j = 0; j < 10; j++) {
             FloorTile curTile = new CornerTile(3);
             curboard[i][j] = (curTile);
@@ -133,15 +202,27 @@ public class Main extends Application {
             imageview.setFitWidth(40); 
             imageview.setPreserveRatio(true);
             game.getChildren().add(imageview);
-            
          }
       }
    
-      
-      
+
       inGameScreen = new Scene(game, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
       game.getChildren().add(pauseButton);
-
+      game.getChildren().add(endTurnButton);
+      game.getChildren().add(drawTile);
+      game.getChildren().add(availableAction);
+      game.getChildren().add(playAction);
+      game.getChildren().add(playerTurnLabel);
+      game.getChildren().add(rotateRight);
+      game.getChildren().add(rotateLeft);
+      drawTile.setOnAction(new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent event) {
+            drawTile.setDisable(true);
+            playAction.setDisable(false);
+            game.getChildren().add(insertPrompt);
+           }
+     });
 
       //PAUSE MENU
 
@@ -174,6 +255,13 @@ public class Main extends Application {
       newButton2.setOnAction(e -> window.setScene(mainMenu));
       leaderboard.getChildren().add(newButton2); //ADDED BUTTON TO TEST NAVIGATION 
 
+      //LOAD GAME 
+      Pane loadGame = new Pane();
+      loadGameScreen = new Scene(loadGame, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+      Button returnM = new Button("Return to Main Menu");
+      returnM.setOnAction(e -> window.setScene(mainMenu));
+      loadGame.getChildren().add(returnM); //ADDED BUTTON TO TEST NAVIGATION 
+
       //SETUP/PROFILE
 
       Pane profileSetup = new Pane();
@@ -201,6 +289,16 @@ public class Main extends Application {
       fourPlayer.setLayoutY(260);
       fourPlayer.setPrefSize(100,20);
       profileSetup.getChildren().add(fourPlayer); 
+
+      fourPlayer.setOnAction(new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent event) {
+            for (int x = 0; x < 4; x++) {
+               System.out.println(x);
+            }
+           }
+
+     });
 
       Button startGame = new Button("Start game");
       startGame.setLayoutX(550);
