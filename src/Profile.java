e815525c398292f4638da9fbcd9c5369aee64a77
MatthewGClarke <74783734+MatthewGClarke@users.noreply.github.com>
2020-12-07 03:12:44 +0000

    Update Profile.java

--- src/Profile.java
+++ src/Profile.java
@@ -10,36 +10,61 @@ public class Profile {
     private int[] losses;
     private String name;
 
+    
+    /**
+     * This the the constructor for the Profile Class
+     * 
+     * @param name Takes the name of the profile in string form
+     * @param NUM_OF_PRESET_BOARDS Takes the number of presetBoards in int form
+     * 			(used for building the array of wins and losses)
+     */
     public Profile(String name, int NUM_OF_PRESET_BOARDS) {
         this.name = name;
         wins = new int[NUM_OF_PRESET_BOARDS];
         losses= new int[NUM_OF_PRESET_BOARDS];
     }
-
-    public int getWins() {
-        return wins[LeaderBoard.getPresetBoard()];
+    
+    /**
+     * Used as a Getter for the wins 
+     * 
+     * @return wins depending on the current board type (presetBoardNum) 
+     */
+    public int getWins(int index) {
+        return wins[index];
     }
     
-    public int[] getAllWins() {
-        return wins;
-    }
-
+    /**
+     * Used as a Setter for the wins 
+     * 
+     * @param wins for the profile in the presetBoard Location. 
+     */
     public void setWins(int wins, int index) {
-    	this.wins[index] = wins;
+        this.wins[index] = wins;
     }
 
-    public int getLosses() {
-        return losses[LeaderBoard.getPresetBoard()];
+    /**
+     * Used as a Getter for the losses 
+     * 
+     * @return losses depending on the current board type (presetBoardNum) 
+     */
+    public int getLosses(int index) {
+        return losses[index];
     }
     
-    public int[] getAllLosses() {
-        return losses;
-    }
-
+    /**
+     * Used as a Setter for the losses 
+     * 
+     * @param losses for the profile in the presetBoard Location. 
+     */
     public void setLosses(int losses, int index) {
         this.losses[index] = losses;
     }
-
+    
+    /**
+     * Used as a Getter for the name 
+     * 
+     * @return name returns the name of the profile
+     */
     public String getName() {
         return name;
     }
@@ -52,3 +77,4 @@ public class Profile {
         this.name = name;
     }
 }
+
